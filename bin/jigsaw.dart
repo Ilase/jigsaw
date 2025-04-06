f5c224e44415d9c51f5316ecccfe6044fa6ce620
Ilase <igeodec@gmail.com>
2025-04-06 23:27:56 +0300

    new class apiService not working correcktly rewrite

--- bin/jigsaw.dart
+++ bin/jigsaw.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:jigsaw/data/j_logger.dart';
+import 'package:jigsaw/domain/api/api_service.dart';
 import 'package:jigsaw/domain/api/config.dart';
 import 'package:jigsaw/domain/api/jwt/jwt_auth.dart';
 import 'package:shelf/shelf.dart';
@@ -14,50 +15,52 @@ void main(List<String> args) async {
   ///Setting up auth middleware
   final auth = Authenticator(jwtSecret: config["secret"]);
 
+  final apiService = ApiService(auth: auth);
+
   jLogger.i("Server configuration: \n $config");
 
-  final api = Router();
-  //Handler for all requests
-  final handler = const Pipeline()
-      .addMiddleware(
-        logRequests(
-          logger: (line, f) {
-            print("$line + || + ${f.toString()}");
-          },
-        ),
-      )
-      .addMiddleware(auth.verifyJWT(excludedPaths: ['/login', '/']))
-      .addHandler(api.call);
+  // final api = Router();
+  // //Handler for all requests
+  // final handler = const Pipeline()
+  //     .addMiddleware(
+  //       logRequests(
+  //         logger: (line, f) {
+  //           print("$line + || + ${f.toString()}");
+  //         },
+  //       ),
+  //     )
+  //     .addMiddleware(auth.verifyJWT(excludedPaths: ['/login', '/']))
+  //     .addHandler(api.call);
 
-  api.post('/login', (Request request) async {
-    final body = await request.readAsString();
-    final data = jsonDecode(body) as Map<String, dynamic>;
+  // api.post('/login', (Request request) async {
+  //   final body = await request.readAsString();
+  //   final data = jsonDecode(body) as Map<String, dynamic>;
+  //
+  //   //Get credentials
+  //   final username = data['username'] as String;
+  //   final password = data['password'] as String;
+  //
+  //   if (!auth.users.containsKey(username) ||
+  //       auth.users[username]!['password'] !=
+  //           Authenticator.hashPassword(password)) {
+  //     return Response.unauthorized('Invalid credentials');
+  //   }
+  //   final user = auth.users[username]!;
+  //   final String role = user['role'] as String;
+  //   final token = auth.generateJWT(username, role);
+  //
+  //   return Response.ok(
+  //     jsonEncode({"jigsawVersion": "0.1.0", "token": token}),
+  //     headers: {'Content-Type': 'application/json'},
+  //   );
+  // });
+  // api.get('/home', (Request request) {
+  //   return Response.ok(
+  //     jsonEncode({"jigsawVersion": "0.1.0"}),
+  //     headers: {'Content-Type': 'application/json'},
+  //   );
+  // });
 
-    //Get credentials
-    final username = data['username'] as String;
-    final password = data['password'] as String;
-
-    if (!auth.users.containsKey(username) ||
-        auth.users[username]!['password'] !=
-            Authenticator.hashPassword(password)) {
-      return Response.unauthorized('Invalid credentials');
-    }
-    final user = auth.users[username]!;
-    final String role = user['role'] as String;
-    final token = auth.generateJWT(username, role);
-
-    return Response.ok(
-      jsonEncode({"jigsawVersion": "0.1.0", "token": token}),
-      headers: {'Content-Type': 'application/json'},
-    );
-  });
-  api.get('/home', (Request request) {
-    return Response.ok(
-      jsonEncode({"jigsawVersion": "0.1.0"}),
-      headers: {'Content-Type': 'application/json'},
-    );
-  });
-
-  final server = await serve(handler, "localhost", config["port"]);
+  final server = await serve(apiService.handler, "localhost", config["port"]);
   print('Server listening on port ${server.port}');
 }
---
+++ lib/domain/api/api_service.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:jigsaw/domain/api/jwt/jwt_auth.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+class ApiService {
+  Authenticator auth;
+  final Router _router;
+  late final handler;
+
+  ApiService({required this.auth}) : _router = Router() {
+    _setupRoutes();
+    handler = const Pipeline()
+        .addMiddleware(
+          logRequests(
+            logger: (line, f) {
+              print("$line + || + ${f.toString()}");
+            },
+          ),
+        )
+        .addMiddleware(auth.verifyJWT(excludedPaths: ['/login', '/']))
+        .addHandler(_router.call);
+    _router.mount('/api/v1', handler);
+  }
+
+  void _setupRoutes() {
+    _router.post('/login', _login);
+    _router.get('/home', _home);
+  }
+
+  Future<Response> _login(Request request) async {
+    final body = await request.readAsString();
+    final data = jsonDecode(body) as Map<String, dynamic>;
+
+    //Get credentials
+    final username = data['username'] as String;
+    final password = data['password'] as String;
+
+    if (!auth.users.containsKey(username) ||
+        auth.users[username]!['password'] !=
+            Authenticator.hashPassword(password)) {
+      return Response.unauthorized('Invalid credentials');
+    }
+    final user = auth.users[username]!;
+    final String role = user['role'] as String;
+    final token = auth.generateJWT(username, role);
+
+    return Response.ok(
+      jsonEncode({"jigsawVersion": "0.1.0", "token": token}),
+      headers: {'Content-Type': 'application/json'},
+    );
+  }
+
+  Future<Response> _home(Request request) async {
+    return Response.ok(jsonEncode({'home': 'this sis'}));
+  }
+}
